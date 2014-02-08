ae3846c208ff87cdb96874800c4ae10ded259551
Leonid Sobolievskyi <leonid@sobolievskyi.name>
2014-02-08 18:45:55 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+gem 'mysql2'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'haml'
+gem 'devise'
+gem 'bootstrap-sass'
+
+group :development do
+  gem 'mailcatcher'
+  gem 'powify'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'database_cleaner'
+  gem 'shoulda-matchers'
+  gem 'shoulda-context'
+  gem 'cucumber'
+end
+
+group :test, :development do
+  gem 'debugger'
+  gem 'pry-debugger'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpdesk::Application.config.secret_key_base = '018ef4b613a5ec964a5831cf46ad9fe2598331cf0cc3571b6f9e78901d762ff5a6da8cb52ae1894d880186520e77b69bd1efeada0ed2a6fb7862b6e29c462207'
